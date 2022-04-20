21cd7167fedff5d6a9d2704fff95611988034509
kkkomAR <100324129+kkkomAR@users.noreply.github.com>
2022-04-20 23:35:48 +0300

    lab3
    lab3

--- fclient2/app/src/main/java/com/example/fclient2/MainActivity.java
+++ fclient2/app/src/main/java/com/example/fclient2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+<<<<<<< Updated upstream
 import android.widget.TextView;
 
 import com.example.fclient2.databinding.ActivityMainBinding;
@@ -16,6 +17,23 @@ public class MainActivity extends AppCompatActivity {
             System.loadLibrary("fclient2");
             System.loadLibrary("mbedcrypto");
         }
+=======
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import android.app.Activity;
+import android.content.Intent;
+
+import org.apache.commons.codec.DecoderException;
+import org.apache.commons.codec.binary.Hex;
+
+public class MainActivity extends AppCompatActivity implements TransactionActivity {
+
+    ActivityResultLauncher<Intent> activityResultLauncher;
+
+>>>>>>> Stashed changes
     static {
         System.loadLibrary("fclient2");
     }
@@ -24,6 +42,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+<<<<<<< Updated upstream
+=======
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        pin = data.getStringExtra("pin");
+                        synchronized (MainActivity.this) {
+                            MainActivity.this.notifyAll();
+                        }
+                    }
+                });
+
+>>>>>>> Stashed changes
         int res = initRng();
         byte[] rnd = randomBytes(16);
 
@@ -36,6 +69,7 @@ public class MainActivity extends AppCompatActivity {
         Log.i("data", Arrays.toString(data));
         byte[] encrypt_data = encrypt(rnd, data);
 
+<<<<<<< Updated upstream
         byte[] decrypt_data = decrypt(rnd, encrypt_data);
 
         Log.i("enc_data", Arrays.toString(encrypt_data));
@@ -43,11 +77,73 @@ public class MainActivity extends AppCompatActivity {
 
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+=======
+    public void onButtonClick(View view) {
+//        new Thread(() -> {
+//            try {
+//                byte[] trd = stringToHex("9F0206000000000100");
+//                transaction(trd);
+//            } catch (Exception exception) {
+//                Log.println(Log.ERROR, "MtLog", Arrays.toString(exception.getStackTrace()));
+//            }
+//        }).start();
+        byte[] trd = stringToHex("9F0206000000000100");
+        transaction(trd);
+    }
+
+    private String pin;
+
+    @Override
+    public String enterPin(int ptc, String amount) {
+        pin = new String();
+
+        Intent intent = new Intent(MainActivity.this, PinpadActivity.class);
+        intent.putExtra("ptc", ptc);
+        intent.putExtra("amount", amount);
+
+        synchronized (MainActivity.this) {
+            activityResultLauncher.launch(intent);
+            try {
+                MainActivity.this.wait();
+            } catch (Exception exception) {
+                Log.println(Log.ERROR, "MtLog", exception.getMessage());
+            }
+        }
+
+        return pin;
+    }
+    @Override
+    public void transactionResult(boolean result) {
+        runOnUiThread(() -> {
+            Toast.makeText(MainActivity.this, result ? "ok" : "failed", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+
+    public static byte[] stringToHex(String s) {
+        byte[] hex;
+        try {
+            hex = Hex.decodeHex(s.toCharArray());
+        } catch (DecoderException ex) {
+            hex = null;
+        }
+        return hex;
+>>>>>>> Stashed changes
     }
 
     public native String stringFromJNI();
     public static native int initRng();
+
     public static native byte[] randomBytes(int no);
+
     public static native byte[] encrypt(byte[] key, byte[] data);
+
     public static native byte[] decrypt(byte[] key, byte[] data);
+<<<<<<< Updated upstream
 }
+=======
+
+    public native boolean transaction(byte[] trd);
+
+}
+>>>>>>> Stashed changes
