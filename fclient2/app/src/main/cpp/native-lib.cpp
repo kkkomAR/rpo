b2fbf86dc52ea8a1fe50e66cdd99ee909455aaff
kkkomAR <100324129+kkkomAR@users.noreply.github.com>
2022-04-28 22:41:56 +0300

    lab6
    lab6

--- fclient2/app/src/main/cpp/native-lib.cpp
+++ fclient2/app/src/main/cpp/native-lib.cpp
@@ -1,101 +1,103 @@
 #include <jni.h>
 #include <string>
-
 #include <android/log.h>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/android_sink.h>
-
-#include "mbedtls/entropy.h"
-#include "mbedtls/ctr_drbg.h"
+#include <mbedtls/entropy.h>
+#include <mbedtls/ctr_drbg.h>
 #include <mbedtls/des.h>
+#define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, "rpolab_ndk", __VA_ARGS__)
+#define SLOG_INFO(...) android_logger->info( __VA_ARGS__ )
 
-mbedtls_entropy_context entropy;
-mbedtls_ctr_drbg_context ctr_drbg;
-char *personalization = "lab-sample-app";
-
-#define LOG_INFO(...) __android_log_print(ANDROID_LOG_INFO, "lab_ndk", __VA_ARGS__)
-#define SLOG_INFO(...) logger->info(__VA_ARGS__)
-
-auto logger = spdlog::android_logger_mt("android", "lab_ndk");
-
-JavaVM *gJvm = nullptr;
-
-JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *pJvm, void *reserved) {
-    gJvm = pJvm;
+JavaVM* gJvm = nullptr;
+JNIEXPORT jint JNICALL JNI_OnLoad (JavaVM* pjvm, void* reserved)
+{
+    gJvm = pjvm;
     return JNI_VERSION_1_6;
 }
 
-JNIEnv *getEnv(bool &detach) {
-    JNIEnv *env = nullptr;
-    int status = gJvm->GetEnv((void **)&env, JNI_VERSION_1_6);
+JNIEnv* getEnv (bool& detach)
+{
+    JNIEnv* env = nullptr;
+    int status = gJvm->GetEnv ((void**)&env, JNI_VERSION_1_6);
     detach = false;
-    if (status == JNI_EDETACHED) {
-        status = gJvm->AttachCurrentThread(&env, nullptr);
-        if (status < 0) {
+    if (status == JNI_EDETACHED)
+    {
+        status = gJvm->AttachCurrentThread (&env, NULL);
+        if (status < 0)
+        {
             return nullptr;
         }
-
         detach = true;
     }
-
     return env;
 }
 
-void releaseEnv(bool detach, JNIEnv* env) {
-    if (detach && (gJvm != nullptr)) {
-        gJvm->DetachCurrentThread();
+void releaseEnv (bool detach, JNIEnv* env)
+{
+    if (detach && (gJvm != nullptr))
+    {
+        gJvm->DetachCurrentThread ();
     }
 }
 
-extern "C" JNIEXPORT jboolean JNICALL
-Java_com_example_fclient2_MainActivity_transaction(JNIEnv *xenv, jobject xthiz, jbyteArray xtrd) {
+mbedtls_entropy_context entropy;
+mbedtls_ctr_drbg_context ctr_drbg;
+char *personalization = "rpolab-sample-app";
+
+auto android_logger = spdlog::android_logger_mt("android", "rpolab_ndk");
+
+extern "C" JNIEXPORT jstring JNICALL
+Java_com_example_rpolab_MainActivity_stringFromJNI(
+        JNIEnv* env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    LOG_INFO("Hello from c++ %d", 2021);
+    SLOG_INFO("Hello from spdlog {0}", 2021);
+    return env->NewStringUTF(hello.c_str());
+}
+
+extern "C"
+JNIEXPORT jboolean JNICALL
+Java_com_example_rpolab_MainActivity_transaction(JNIEnv *xenv, jobject xthiz, jbyteArray xtrd) {
     jobject thiz = xenv->NewGlobalRef(xthiz);
-    jbyteArray trd = (jbyteArray)xenv->NewGlobalRef(xtrd);
+    jbyteArray trd = (jbyteArray) xenv->NewGlobalRef(xtrd);
 
     std::thread t([thiz, trd] {
         bool detach = false;
         JNIEnv *env = getEnv(detach);
         jclass cls = env->GetObjectClass(thiz);
         jmethodID id = env->GetMethodID(cls, "enterPin", "(ILjava/lang/String;)Ljava/lang/String;");
-
-        uint8_t *p = (uint8_t*)env->GetByteArrayElements(trd, 0);
+        //TRD 9F0206000000000100 = amount = 1р
+        uint8_t *p = (uint8_t *) env->GetByteArrayElements(trd, 0);
         jsize sz = env->GetArrayLength(trd);
-
-        if ((sz != 9) || (p[0] != 0x9F) || (p[1] != 0x02) || (p[2] != 0x06)) {
+        if ((sz != 9) || (p[0] != 0x9F) || (p[1] != 0x02) || (p[2] != 0x06))
             return false;
-        }
 
-        char buffer[13];
-        for (int i = 0; i < 6; ++i) {
+        char buf[13];
+        for (int i = 0; i < 6; i++) {
             uint8_t n = *(p + 3 + i);
-            buffer[i * 2] = ((n & 0xF0) >> 4) + '0';
-            buffer[i * 2 + 1] = (n & 0x0F) + '0';
+            buf[i * 2] = ((n & 0xF0) >> 4) + '0';
+            buf[i * 2 + 1] = (n & 0x0F) + '0';
         }
+        buf[12] = 0x00;
+        jstring jamount = (jstring) env->NewStringUTF(buf);
 
-        buffer[12] = 0x00;
-        jstring amount = (jstring) env->NewStringUTF(buffer);
         int ptc = 3;
         while (ptc > 0) {
-            jstring pin = (jstring) env->CallObjectMethod(thiz, id, ptc, amount);
+            jstring pin = (jstring) env->CallObjectMethod(thiz, id, ptc, jamount);
             const char *utf = env->GetStringUTFChars(pin, nullptr);
-
             env->ReleaseStringUTFChars(pin, utf);
-            if ((utf != nullptr) && (strcmp(utf, "1234") == 0)) {
+            if ((utf != nullptr) && (strcmp(utf, "1234") == 0))
                 break;
-            }
-
             ptc--;
         }
 
-
         id = env->GetMethodID(cls, "transactionResult", "(Z)V");
         env->CallVoidMethod(thiz, id, ptc > 0);
-
-        env->ReleaseByteArrayElements(trd, (jbyte *)p, 0);
-
+        env->ReleaseByteArrayElements(trd, (jbyte *) p, 0);
         env->DeleteGlobalRef(thiz);
         env->DeleteGlobalRef(trd);
-
         releaseEnv(detach, env);
         return true;
     });
@@ -103,28 +105,17 @@ Java_com_example_fclient2_MainActivity_transaction(JNIEnv *xenv, jobject xthiz, 
     t.detach();
     return true;
 }
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_fclient2_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    LOG_INFO("Hello from c++ %d", 2022);
-    logger->info("Hello from spdlog {0}", 2022);
-    return env->NewStringUTF(hello.c_str());
-}
 
 extern "C" JNIEXPORT jint JNICALL
-Java_com_example_fclient2_MainActivity_initRng(JNIEnv *env, jclass clazz) {
+Java_com_example_rpolab_MainActivity_initRng(JNIEnv *env, jclass clazz) {
     mbedtls_entropy_init( &entropy );
     mbedtls_ctr_drbg_init( &ctr_drbg );
-
     return mbedtls_ctr_drbg_seed( &ctr_drbg , mbedtls_entropy_func, &entropy,
                                   (const unsigned char *) personalization,
                                   strlen( personalization ) );
 }
-
 extern "C" JNIEXPORT jbyteArray JNICALL
-Java_com_example_fclient2_MainActivity_randomBytes(JNIEnv *env, jclass, jint no) {
+Java_com_example_rpolab_MainActivity_randomBytes(JNIEnv *env, jclass, jint no) {
     uint8_t * buf = new uint8_t [no];
     mbedtls_ctr_drbg_random(&ctr_drbg, buf, no);
     jbyteArray rnd = env->NewByteArray(no);
@@ -133,10 +124,9 @@ Java_com_example_fclient2_MainActivity_randomBytes(JNIEnv *env, jclass, jint no)
     return rnd;
 }
 
+// https://tls.mbed.org/api/des_8h.html
 extern "C" JNIEXPORT jbyteArray JNICALL
-Java_com_example_fclient2_MainActivity_encrypt(JNIEnv *env, jclass, jbyteArray key, jbyteArray data)
-{
-
+Java_com_example_rpolab_MainActivity_encrypt(JNIEnv *env, jclass, jbyteArray key, jbyteArray data) {
     jsize ksz = env->GetArrayLength(key);
     jsize dsz = env->GetArrayLength(data);
     if ((ksz != 16) || (dsz <= 0)) {
@@ -144,10 +134,8 @@ Java_com_example_fclient2_MainActivity_encrypt(JNIEnv *env, jclass, jbyteArray k
     }
     mbedtls_des3_context ctx;
     mbedtls_des3_init(&ctx);
-
     jbyte * pkey = env->GetByteArrayElements(key, 0);
-
-    // PKCS#5 padding
+    // Паддинг PKCS#5
     int rst = dsz % 8;
     int sz = dsz + 8 - rst;
     uint8_t * buf = new uint8_t[sz];
@@ -167,10 +155,8 @@ Java_com_example_fclient2_MainActivity_encrypt(JNIEnv *env, jclass, jbyteArray k
     return dout;
 }
 
-
 extern "C" JNIEXPORT jbyteArray JNICALL
-Java_com_example_fclient2_MainActivity_decrypt(JNIEnv *env, jclass, jbyteArray key, jbyteArray data)
-{
+Java_com_example_rpolab_MainActivity_decrypt(JNIEnv *env, jclass, jbyteArray key, jbyteArray data) {
     jsize ksz = env->GetArrayLength(key);
     jsize dsz = env->GetArrayLength(data);
     if ((ksz != 16) || (dsz <= 0) || ((dsz % 8) != 0)) {
@@ -178,21 +164,16 @@ Java_com_example_fclient2_MainActivity_decrypt(JNIEnv *env, jclass, jbyteArray k
     }
     mbedtls_des3_context ctx;
     mbedtls_des3_init(&ctx);
-
     jbyte * pkey = env->GetByteArrayElements(key, 0);
-
     uint8_t * buf = new uint8_t[dsz];
-
     jbyte * pdata = env->GetByteArrayElements(data, 0);
     std::copy(pdata, pdata + dsz, buf);
     mbedtls_des3_set2key_dec(&ctx, (uint8_t *)pkey);
     int cn = dsz / 8;
     for (int i = 0; i < cn; i++)
         mbedtls_des3_crypt_ecb(&ctx, buf + i*8, buf +i*8);
-
-    //PKCS#5.simplified. need to check every padded byte for security reasons
+    //PKCS#5. упрощено. по соображениям безопасности надо проверить каждый бай
     int sz = dsz - 8 + buf[dsz-1];
-
     jbyteArray dout = env->NewByteArray(sz);
     env->SetByteArrayRegion(dout, 0, sz, (jbyte *)buf);
     delete[] buf;
