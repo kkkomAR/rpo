87ee606f8365a8062c89c73fe73ff7adb50b2cc3
kkkomAR <100324129+kkkomAR@users.noreply.github.com>
2022-05-06 22:01:30 +0300

    lab12
    lab12

---
+++ front/backend/src/main/java/com/example/backend/controllers/CountryController.java
@@ -0,0 +1,99 @@
+package com.example.backend.controllers;
+
+import com.example.backend.models.Artist;
+import com.example.backend.tools.DataValidationException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.example.backend.models.Country;
+import com.example.backend.repositories.CountryRepository;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.*;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1")
+public class CountryController {
+    @Autowired
+    CountryRepository countryRepository;
+
+    @GetMapping("/countries/{id}")
+    public ResponseEntity<Country> getCountry(@PathVariable(value = "id") Long countryId)
+            throws DataValidationException {
+        Country country = countryRepository.findById(countryId)
+                .orElseThrow(()-> new DataValidationException("Страна с таким индексом не найдена"));
+        return ResponseEntity.ok(country);
+    }
+
+    @GetMapping("/countries/{id}/artists")
+    public ResponseEntity<List<Artist>> getCountryArtists(@PathVariable(value = "id") Long countryId) {
+            Optional<Country> cc = countryRepository.findById(countryId);
+            if (cc.isPresent()) return ResponseEntity.ok(cc.get().artists);
+            return ResponseEntity.ok(new ArrayList<Artist>());
+    }
+
+    @GetMapping("/countries")
+    public Page<Country> getAllCountries(@RequestParam("page") int page, @RequestParam("limit") int limit) {
+        return countryRepository.findAll(PageRequest.of(page, limit, Sort.by(Sort.Direction.ASC, "name")));
+    }
+
+    @PostMapping("/countries")
+    public ResponseEntity<Object> createCountry(@Valid @RequestBody Country country)
+            throws DataValidationException {
+        try {
+            Country nc = countryRepository.save(country);
+            return new ResponseEntity<Object>(nc, HttpStatus.OK);
+        }
+        catch(Exception ex) {
+            String error;
+            if (ex.getMessage().contains("countries.name_UNIQUE"))
+                throw new DataValidationException("Эта страна уже есть в базе");
+            else
+                throw new DataValidationException("Неизвестная ошибка");
+        } }
+
+    @PutMapping("/countries/{id}")
+    public ResponseEntity<Country> updateCountry(@PathVariable(value = "id") Long countryId,
+                                                 @Valid @RequestBody Country countryDetails)
+            throws DataValidationException {
+        try {
+            Country country = countryRepository.findById(countryId)
+                    .orElseThrow(() -> new DataValidationException("Страна с таким индексом не найдена"));
+            country.name = countryDetails.name;
+            countryRepository.save(country);
+            return ResponseEntity.ok(country);
+        }
+        catch (Exception ex) {
+            String error;
+            if (ex.getMessage().contains("countries.name_UNIQUE"))
+                throw new DataValidationException("Данная страна уже есть в базе");
+            else
+                throw new DataValidationException("Неизвестная ошибка");
+        }
+    }
+
+    @PostMapping("/deletecountries")
+    public ResponseEntity deleteCountries(@Valid @RequestBody List<Country> countries) {
+        countryRepository.deleteAll(countries);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @DeleteMapping("/countries/{id}")
+    public Map<String, Boolean> deleteCountry(@PathVariable(value = "id") Long countryId) {
+        Optional<Country> country = countryRepository.findById(countryId);
+        Map<String, Boolean> resp = new HashMap<>();
+
+        if (country.isPresent()) {
+            countryRepository.delete(country.get());
+            resp.put("deleted", Boolean.TRUE);
+        } else resp.put("deleted", Boolean.FALSE);
+
+        return resp;
+    }
+}
